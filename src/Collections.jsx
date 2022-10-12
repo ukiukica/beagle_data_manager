62e3bbf1a3e6fbf6afd0719d1481ad09dcfd062d
uki <ukiworks034@gmail.com>
2022-10-12 09:37:35 -0400

    collections now fetched from API

--- src/Collections.jsx
+++ src/Collections.jsx
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SpecList from "./SpecList";
 
 function Collections() {
+  const [specs, setSpecs] = useState();
   const [specType, setSpecType] = useState("user");
+
+  console.log("specs", specs)
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://beagleschema.demcrepl.repl.co/specs");
+      const data = await res.json();
+      // console.log("data", data);
+      setSpecs(data["specs"]);
+    }
+
+    fetchData();
+  }, []);
+
+  const formatSpec = (spec) => {
+    let formattedSpec = spec;
+    if (formattedSpec.includes("_")) {
+      formattedSpec = formattedSpec.split("_").join(" ");
+    }
+    return formattedSpec.toUpperCase()
+  }
 
   return (
     <>
       <div id="collections">
-        <div onClick={() => setSpecType("user")}>Users</div>
-        <div onClick={() => setSpecType("customer")}>Customers</div>
-        <div onClick={() => setSpecType("order")}>Orders</div>
-        <div onClick={() => setSpecType("shopping_cart")}>Shopping Carts</div>
+        {specs && specs.map((spec) => (
+          <div key={spec} onClick={() => setSpecType(spec)}>{formatSpec(spec)}</div>
+        ))}
       </div>
-      <br />
 
       <div id="spec-list">
         <SpecList specType={specType} />
